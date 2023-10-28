01f9a7bf49abec7cedc0f6aef2fe0d7ab23c8054
ruby <nogood1014@naver.com>
2023-10-27 23:38:59 -0700

    fastlms maven 초기화 설정

---
+++ src/main/java/com/zerobase/fastlms/fastlms/FastlmsApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.fastlms.fastlms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastlmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastlmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/fastlms/fastlms/FastlmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.fastlms.fastlms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastlmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
